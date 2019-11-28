858ef76657cb57bcd35aeef1fe515c7a8e87bb88
Rasmus Karling <rasmusjk@metropolia.fi>
2019-11-28 16:28:29 +0200

    added user post

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
---
+++ model/userModel.js
@@ -0,0 +1,39 @@
+const pool = require('../database/db');
+const promisePool = pool.promise();
+
+const getAllUsers = async () => {
+  try {
+    const [rows] = await promisePool.query('SELECT * FROM user');
+    return rows;
+  } catch (e) {
+    console.log('error', e.message);
+  }
+};
+
+const getUser = async (id) => {
+  try {
+    const [row] = await promisePool.execute(
+        'SELECT * FROM user WHERE user_id = ?;', id);
+    return row;
+  } catch (e) {
+    console.log('error', e.message);
+    return {error: 'error in db'};
+  }
+};
+
+const addUser = async (username, email, passwd) => {
+  try {
+    const  [row]= await promisePool.execute(
+        'INSERT INTO user(user_name, user_email, user_passwd) Values("'+username+'", "'+email+'", "'+passwd+'")');
+    return row;
+  } catch (e) {
+    console.error(e);
+    return {error: 'error in db'};
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  getUser,
+  addUser
+};
