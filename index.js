f8ade7ee8614fc13c145ede4f0f8518694cc4127
matiasjj <matiasjj@metropolia.fi>
2019-11-28 16:22:27 +0200

    started the css based on rough plannings

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
+const pool = require('../database/db');
+
 const app = express();
 const port = 3000;
 
-
-const pool = require('../database/db');
 const promisePool = pool.promise();
-
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
