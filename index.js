26840e0a46092c9a7d914f706c866cc0273ef843
Matias Jalava <matias@elm.fi>
2019-12-02 18:46:35 +0200

    nothing changed, needed to complete a merge

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const pool = require('../database/db');
-
 const app = express();
 const port = 3000;
 
-const promisePool = pool.promise();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+const userRoute = require('./routes/userRoute');
+app.use('/user',userRoute);
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
