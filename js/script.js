18d30e09f4cb7f85488bf6a22ada853251dd8e70
Matias Jalava <matias@elm.fi>
2019-12-02 18:35:44 +0200

    better searchfield etc

--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@
 
 console.log('script loaded');
 
-const menuButton = document.getElementById('menuButton');
+const searchButton = document.getElementById('searchButton');
+const arrowButton = document.getElementById('searchFieldExpander');
 const checkBox1 = document.getElementById('toggle-1');
 const logo = document.getElementById('mainTitle');
 const searchField = document.getElementById('searchField');
 const headerLeft = document.getElementById('headerLeft');
 const headerRight = document.getElementById('headerRight');
 
+//only a debugging function
 const checkIfTrue = (checkBox) => {
     if(checkBox){
         return true
@@ -17,22 +19,34 @@ const checkIfTrue = (checkBox) => {
     }
 };
 
-menuButton.onclick=()=>{
+arrowButton.onclick=()=>{
     console.log(checkIfTrue(checkBox1.checked));
     checkBox1.checked = !checkBox1.checked;
 
     if(checkBox1.checked){
         logo.style.fontSize=('0');
         searchField.style.visibility=('initial');
-        searchField.style.width=('90%');
+        searchField.style.righ=('15ch');
+        searchField.style.width=('35ch');
         headerLeft.style.width=('30%');
         headerRight.style.width=('70%');
+        arrowButton.style.left=('0');
+        arrowButton.style.transform='rotate(180deg)';
     }else{
-        logo.style.fontSize=('var(--text-title-size)');
-        searchField.style.visibility=('hidden');
+        logo.style.fontSize=('var(--text-title1-size)');
+        searchField.style.width=('0');
         headerLeft.style.width=('50%');
         headerRight.style.width=('50%');
+        arrowButton.style.right=('18ch');
+        arrowButton.style.left=('');
+        arrowButton.style.transform='';
     }
+};
+
+searchButton.onclick=()=>{
+    const searchParams = searchField.value;
+    console.log(searchParams);
+    searchField.value=('');
 };
 
 //.addClass
