cf5023c1467c7567d9bd82c3dbb34c7de78afe3d
Rasmus Karling <rasmusjk@metropolia.fi>
2019-12-12 21:29:27 +0200

    changed routes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*
-* Main script for the app backen. It defines dependencies and routing. It starts all main functions for backend.
+* Main script for the app backend. It defines dependencies and routing. It starts all main functions for backend.
 * Also functionality of passport authentication and user serialization is here.
 * */
 
@@ -65,26 +65,26 @@ passport.deserializeUser((user_id, done) => {
     done(null, {user_id: user_id});
 });
 
-app.post('/login',
+app.post('/app/login',
     passport.authenticate('local', {
-        successRedirect: '/loginSuccess',
-        failureRedirect: '/loginError'
+        successRedirect: '/app/loginSuccess',
+        failureRedirect: '/app/loginError'
     })
 );
 
-app.get('/loginSuccess', (req, res) => {
-    res.redirect('/index.html');
+app.get('/app/loginSuccess', (req, res) => {
+    res.redirect('/app/index.html');
 });
 
-app.get('/index', (req, res) => {
-    res.redirect('/index.html');
+app.get('/app/index', (req, res) => {
+    res.redirect('/app/index.html');
 });
 
 app.get('/loginError', (req, res) => {
     res.redirect('/html/login.html');
 });
 
-app.get('/', (req, res) => {
+app.get('/app', (req, res) => {
     res.redirect('/html/login.html');
 });
 
